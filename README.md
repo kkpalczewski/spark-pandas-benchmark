04e61e13470df1009432463ae747208b8650b651
kp <analytics@sellectra.com>
2022-08-04 11:07:29 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Some pyspark vs pandas benchmarks
+## Data
+https://drive.google.com/file/d/1kCXnIeoPT6p9kS_ANJ0mmpxlfDwK1yio/view
---
+++ warm_up_1.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+
+def warm_up_1(session: SparkSession):
+    """
+    Find out how many orders, how many products and how many sellers are in the data.
+    How many products have been sold at least once?
+    Which is the product contained in more orders?
+    :param session:
+    :return:
+    """
+    products = session.read.parquet("data/products_parquet")
+    sales = session.read.parquet("data/sales_parquet")
+    sellers = session.read.parquet("data/sellers_parquet")
+
+    products_count = products.count()
+    sellers_count = sellers.count()
+
+    products_sold = sales.select("product_id").distinct().count()
+
+    products_most_orders = sales.\
+        groupby("product_id").\
+        count().\
+        sort(F.col("count").desc()).\
+        limit(1)
+    print(f"Products count {products_count}, Sellers count: {sellers_count}!")
+    print(f"Products sold at least once: {products_sold}!")
+    print(f"Products most orders: {products_most_orders.take(1)}!")
+
+
+if __name__ == '__main__':
+    spark = SparkSession.builder \
+        .master("local") \
+        .config("spark.sql.autoBroadcastJoinThreshold", -1) \
+        .config("spark.executor.memory", "500mb") \
+        .appName("Exercise1") \
+        .getOrCreate()
+
+    warm_up_1(spark)
